39a564569d5788db36205831775e9fbffe4d7c72
bradreeder <bradjohnreeder@gmail.com>
2016-06-27 16:20:13 +0100

    sent first response from server

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var http = require('http');
 
-var server = http.createServer();
+var message = "Happy happy joy joy";
+
+function handler(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(message);
+  response.end();
+}
+
+var server = http.createServer(handler);
 
 server.listen(3000, function(){
   console.log("Server is running on port 3000. Ready to accept requests!");
