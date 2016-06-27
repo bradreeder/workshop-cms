48019f208fbc0c1284b4b02e08ab55d1503d7fd8
bradreeder <bradjohnreeder@gmail.com>
2016-06-28 00:35:23 +0100

    deleted files for CMS project

--- server.js
+++ server.js
@@ -1,8 +1,1 @@
-var myModule = require('./src/handlers.js');
-var http = require('http');
 
-var server = http.createServer(myModule);
-
-server.listen(3000, function(){
-  console.log("Server is running on port 3000. Ready to accept requests!");
-});
